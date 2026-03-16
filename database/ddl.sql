f8dac032fbb5f3e90c0807bb1ffdb3e6995b8689
mariabfer <maria.barbosa@aluno.ufca.edu.br>
2026-03-16 00:38:07 -0300

    alterado oque ana alice pedou em ligaçao, e incluido as tabelas faladas na ligaçao

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,3 +1,17 @@
+DROP TABLE agendamento;
+
+CREATE TABLE agendamento(
+    id_agendamento INTEGER PRIMARY KEY AUTOINCREMENT,
+    num_sus INTEGER NOT NULL,
+    data_solicitacao TEXT,
+    status TEXT DEFAULT 'PENDENTE',
+    hora_agendamento TEXT,
+    posicao_atual INTEGER,
+    prioridade_calculada INTEGER DEFAULT 0,
+    motivo_prioridade TEXT,
+    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus)
+);
+
 DROP TABLE anamnese;
 
 CREATE TABLE anamnese (
@@ -7,28 +21,34 @@ CREATE TABLE anamnese (
     peso REAL NOT NULL,
     altura REAL NOT NULL,
     presao_arterial REAL NOT NULL,
-    
-    FOREIGN KEY (num_sus) REFERENCES cidadao (num_sus),
-    FOREIGN KEY (id_consulta) REFERENCES consulta (id_consulta)
+    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus),
+    FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
 );
 
 DROP TABLE cidadao;
 
 CREATE TABLE cidadao (
-    num_sus INTEGER PRIMARY KEY NOT NULL ,
+    num_sus INTEGER PRIMARY KEY,
     data_nascimento TEXT NOT NULL,
     genero TEXT CHECK (genero = 'F' OR genero = 'M'),
     naturalidade TEXT,
+    ocupacao TEXT,
     id_endereco INTEGER NOT NULL,
-    cpf_pessoa TEXT NOT NULL,
-    
+    id_pessoa INTEGER NOT NULL,
     FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco),
-    FOREIGN KEY (cpf_pessoa) REFERENCES pessoa(cpf_pessoa),
-    UNIQUE (cpf_pessoa)
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    UNIQUE (id_pessoa)
 );
--- criar tababela responsavel para guardar os dados do responsável legal do cidadão, caso seja menor de idade ou incapaz
---ALTER TABLE cidadao
---ADD ESTADO_CIVIL TEXT NOT NULL OK EM PESSOA 
+
+DROP TABLE cidadao_grupo;
+
+CREATE TABLE cidadao_grupo(
+    num_sus INTEGER NOT NULL,
+    id_grupo INTEGER NOT NULL,
+    PRIMARY KEY (num_sus, id_grupo),
+    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus),
+    FOREIGN KEY (id_grupo) REFERENCES grupo_vulneravel(id_grupo)
+);
 
 DROP TABLE consulta;
 
@@ -38,12 +58,32 @@ CREATE TABLE consulta (
     crm INTEGER NOT NULL,
     id_ubs INTEGER NOT NULL,
     motivo TEXT,
-    resultado TEXT,
-    data_hora TEXT,
-
+    habito_vida TEXT,
+    data TEXT NOT NULL,
     FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus),
     FOREIGN KEY (crm) REFERENCES medico(crm),
     FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs)
+);
+
+DROP TABLE dependente;
+
+CREATE TABLE dependente(
+    id_responsavel INTEGER NOT NULL,
+    id_dependente INTEGER NOT NULL,
+    PRIMARY KEY(id_responsavel,id_dependente),
+    FOREIGN KEY (id_responsavel) REFERENCES pessoa(id_pessoa),
+    FOREIGN KEY (id_dependente) REFERENCES pessoa(id_pessoa)
+);
+
+DROP TABLE documento;
+
+CREATE TABLE documento (
+    id_documento INTEGER PRIMARY KEY AUTOINCREMENT,
+    tipo_documento TEXT NOT NULL,
+    numero_documento TEXT NOT NULL,
+    id_pessoa INTEGER NOT NULL,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    UNIQUE (tipo_documento, numero_documento)
 );
 
 DROP TABLE email;
@@ -52,14 +92,13 @@ CREATE TABLE email (
     id_email INTEGER PRIMARY KEY AUTOINCREMENT,
     endereco_email TEXT NOT NULL,
     id_ubs INTEGER,
-    cpf_pessoa TEXT,
-
+    id_pessoa INTEGER,
     FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs),
-    FOREIGN KEY (cpf_pessoa) REFERENCES pessoa(cpf_pessoa),
-    UNIQUE (endereco_email)
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    UNIQUE (endereco_email),
     CHECK (
-        (id_ubs IS NOT NULL AND cpf_pessoa IS NULL) OR
-        (id_ubs IS NULL AND cpf_pessoa IS NOT NULL)
+        (id_ubs IS NOT NULL AND id_pessoa IS NULL) OR
+        (id_ubs IS NULL AND id_pessoa IS NOT NULL)
     )
 );
 
@@ -73,18 +112,16 @@ CREATE TABLE endereco (
     cidade TEXT,
     estado TEXT,
     cep TEXT NOT NULL,
-
-    UNIQUE (rua, numero, bairro, cidade, cep)
+    UNIQUE (rua, numero, bairro, cidade, estado, cep)
 );
 
 DROP TABLE enfermeiro;
 
 CREATE TABLE enfermeiro (
     cip TEXT PRIMARY KEY,
-    cpf_pessoa TEXT NOT NULL,
-
-    FOREIGN KEY (cpf_pessoa) REFERENCES pessoa(cpf_pessoa)
-    UNIQUE (cpf_pessoa)
+    id_pessoa INTEGER NOT NULL,
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    UNIQUE (id_pessoa)
 );
 
 DROP TABLE exame;
@@ -95,8 +132,7 @@ CREATE TABLE exame (
     nome TEXT NOT NULL,
     tipo TEXT NOT NULL,
     grau_urgencia NUMERIC,
-    status TEXT DEAFULT 'Solicitado',
-
+    status TEXT DEFAULT 'Solicitado',
     FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
 );
 
@@ -109,37 +145,81 @@ CREATE TABLE fila (
     id_ubs INTEGER NOT NULL,
     tipo_atendimento TEXT NOT NULL,
     quantidade_maxima INTEGER NOT NULL,
-    crm INTEGER ,
-    id_vacina INTEGER ,
-    FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs)
+    crm INTEGER,
+    id_vacina INTEGER,
+    FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs),
     FOREIGN KEY (crm) REFERENCES medico(crm),
     FOREIGN KEY (id_vacina) REFERENCES vacina(id_vacina),
     CHECK (
         (crm IS NOT NULL AND id_vacina IS NULL) OR
-        (id_vacina IS NULL AND crm IS NOT NULL)
+        (id_vacina IS NOT NULL AND crm IS NULL)
     )
 
 );
 
-CREATE TABLE agendamento(
-    id_agendamento INTEGER PRIMARY KEY AUTOINCREMENT,
-    num_sus INTEGER NOT NULL,
-    data_solicitacao TEXT,
-    status TEXT DEFAULT 'PENDENTE',
-    hora_agendamento TEXT, 
-    posicao atual INTEGER,
-    prioridade_calculada INTEGER DEFAULT 0,
-    motivo_prioridade TEXT,
 
-    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus)  
-);
+DROP TABLE fila_agendamento;
 
 CREATE TABLE fila_agendamento(
     id_fila INTEGER NOT NULL,
     id_agendamento INTEGER NOT NULL,
     PRIMARY KEY (id_fila, id_agendamento),
-     FOREIGN KEY (id_fila) REFERENCES fila(id_fila),
-     FOREIGN KEY (id_agendamento) REFERENCES agendamento(id_agendamento)
+    FOREIGN KEY (id_fila) REFERENCES fila(id_fila),
+    FOREIGN KEY (id_agendamento) REFERENCES agendamento(id_agendamento)
+);
+
+DROP TABLE grupo_vulneravel;
+
+CREATE TABLE grupo_vulneravel(
+    id_grupo INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_grupo TEXT NOT NULL,
+    peso_prioridade INTEGER NOT NULL,
+    descricao TEXT,
+    UNIQUE(nome_grupo)
+);
+
+DROP TABLE hipotese;
+
+CREATE TABLE hipotese(
+    id_hipotese INTEGER PRIMARY KEY,
+    id_consulta INTEGER NOT NULL,
+    doenca TEXT NOT NULL,
+    cid TEXT NOT NULL,
+    FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
+);
+
+DROP TABLE medicamento;
+
+CREATE TABLE medicamento(
+    id_medicamento INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome_medicamento TEXT NOT NULL UNIQUE,
+    dose REAL NOT NULL,
+    via TEXT NOT NULL
+);
+
+DROP TABLE medicamento_consulta;
+
+CREATE TABLE medicamento_consulta(
+    id_medicamento INTEGER NOT NULL,
+    id_consulta INTEGER NOT NULL,
+    frequencia INTEGER,
+    duracao INTEGER,
+    PRIMARY KEY (id_medicamento, id_consulta),
+    FOREIGN KEY (id_medicamento) REFERENCES medicamento(id_medicamento),
+    FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
+);
+
+DROP TABLE medicamento_ubs;
+
+CREATE TABLE medicamento_ubs(
+    id_medicamento_ubs INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_medicamento INTEGER NOT NULL,
+    id_ubs INTEGER NOT NULL,
+    num_lote TEXT NOT NULL,
+    quantidade_disponivel INTEGER DEFAULT 0,
+    validade TEXT NOT NULL,
+    FOREIGN KEY (id_medicamento) REFERENCES medicamento(id_medicamento),
+    FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs)
 );
 
 DROP TABLE medico;
@@ -147,17 +227,17 @@ DROP TABLE medico;
 CREATE TABLE medico (
     crm INTEGER PRIMARY KEY,
     especialidade TEXT NOT NULL,
-    cpf_pessoa TEXT NOT NULL,
+    id_pessoa INTEGER NOT NULL,
 
-    FOREIGN KEY (cpf_pessoa) REFERENCES pessoa(cpf_pessoa),
-    UNIQUE (cpf_pessoa)
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    UNIQUE (id_pessoa)
 );
 
 
 DROP TABLE pessoa;
 
 CREATE TABLE pessoa(
-    cpf_pessoa INTEGER PRIMARY KEY NOT NULL CHECK(length(cpf_pessoa) = 11),
+    id_pessoa INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_pessoa TEXT NOT NULL,
     id_ubs INTEGER NOT NULL,
     estado_civil TEXT NOT NULL,
@@ -187,14 +267,13 @@ CREATE TABLE telefone (
     id_tel INTEGER PRIMARY KEY AUTOINCREMENT,
     num_telefone TEXT NOT NULL,
     id_ubs INTEGER,
-    cpf_pessoa TEXT,
-
+    id_pessoa INTEGER,
     FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs),
-    FOREIGN KEY (cpf_pessoa) REFERENCES pessoa(cpf_pessoa),
-    UNIQUE (num_telefone) 
+    FOREIGN KEY (id_pessoa) REFERENCES pessoa(id_pessoa),
+    UNIQUE (num_telefone),
     CHECK (
-        (id_ubs IS NOT NULL AND cpf_pessoa IS NULL) OR
-        (id_ubs IS NULL AND cpf_pessoa IS NOT NULL)
+        (id_ubs IS NOT NULL AND id_pessoa IS NULL) OR
+        (id_ubs IS NULL AND id_pessoa IS NOT NULL)
     )
 );
 
@@ -210,30 +289,33 @@ CREATE TABLE ubs (
 
 DROP TABLE vacina;
 
-CREATE TABLE vacina (
+CREATE TABLE vacina(
     id_vacina INTEGER PRIMARY KEY AUTOINCREMENT,
-    tipo TEXT NOT NULL,
-    previne TEXT NOT NULL,
-    quantidade_disponivel NUMERIC DEFAULT 0,
-    id_ubs INTEGER REFERENCES ubs(id_ubs),
-    lote TEXT NOT NULL,
-
-    UNIQUE(tipo, id_ubs),
-    FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs)
+    tipo TEXT NOT NULL UNIQUE,
+    previne TEXT NOT NULL
 );
 
+DROP TABLE vacina_grupo;
 
-CREATE TABLE grupo_vulneravel(
-    id_grupo INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome_grupo TEXT NOT NULL, 
-	peso_prioridade
-);
-
-CREATE TABLE cidadao_grupo(
-    num_sus INTEGER NOT NULL,
+CREATE TABLE vacina_grupo(
+    id_vacina INTEGER NOT NULL,
     id_grupo INTEGER NOT NULL,
-    PRIMARY KEY (num_sus, id_grupo),
+    PRIMARY KEY (id_vacina, id_grupo),
 
-    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus),
+    FOREIGN KEY (id_vacina) REFERENCES vacina(id_vacina),
     FOREIGN KEY (id_grupo) REFERENCES grupo_vulneravel(id_grupo)
+);
+
+DROP TABLE vacina_ubs;
+
+CREATE TABLE vacina_ubs(
+    id_medicamento_ubs INTEGER PRIMARY KEY AUTOINCREMENT,
+    id_vacina INTEGER NOT NULL,
+    id_ubs INTEGER NOT NULL,
+    num_lote TEXT NOT NULL,
+    quantidade_disponivel INTEGER DEFAULT 0,
+    validade TEXT NOT NULL,
+    
+    FOREIGN KEY (id_vacina) REFERENCES vacina(id_vacina),
+    FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs)
 );
