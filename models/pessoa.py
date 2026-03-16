634732a069941409bc4695bbad48cdf7d9b35306
Alana Silva <alanaclara93@gmail.com>
2026-03-16 20:20:53 -0300

    Criado a entidade documento e atualizado as classes pessoa, cidadao, medico e enfermeiro

--- models/pessoa.py
+++ models/pessoa.py
@@ -1,35 +1,52 @@
 from models.ubs import Ubs
+from models.documento import Documento
 
 class Pessoa:
-  def __init__(self, cpf, nome, ubs: Ubs):
-    self.cpf = cpf
-    self.nome = nome
+  def __init__(self, nome_pessoa, estado_civil, ubs: Ubs):
+    self.nome_pessoa = nome_pessoa
     self.emails = []
     self.telefones = []
+    self.documentos = []
+    self.estado_civil = estado_civil
     self.ubs = ubs
+    
+  #Permanecer assim para não resultar em fluxo circular, já que nos respectivos arquivos de telefone e email já importam pessoa
 
+  #O que é passado para este método não é um objeto do tipo Email -> (String)
   def adicionar_email(self, email):
     self.emails.append(email)
-
+  
   def exibir_emails(self):
     print("--- Emails ---")
     for email in self.emails:
-      print(f"Email: {email.email}")
-    print("----------------")
+      print(f"Email: {email}")
+    print("---------------")
 
+  #O que é passado para este método não é um objeto do tipo Telefone -> (Int)
   def adicionar_telefone(self, telefone):
     self.telefones.append(telefone)
 
   def exibir_telefones(self):
     print("--- Telefones ---")
     for telefone in self.telefones:
-      print(f"Telefone:{telefone.num_telefone}")
+      print(f"Telefone: {telefone}")
     print("----------------")
+    
+  def adicionar_documento(self, documento: Documento):
+    self.documentos.append(documento)
+  
+  def exibir_documentos(self):
+    print("--- Documentos ---")
+    for documento in self.documentos:
+      documento.exibir()
 
   def exibir(self):
     print("--- Pessoa: ---")
-    print(f"Nome: {self.nome}")
-    print(f"CPF: {self.cpf}")
+    print(f"Nome: {self.nome_pessoa}")
+    print(f"Estado Cívil: {self.estado_civil}")
+    print()
+    
+    self.exibir_documentos()
     print()
     
     self.exibir_emails()
