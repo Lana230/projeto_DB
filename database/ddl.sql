1e23072ed3eea3e78fae18fd42d70968d7655802
mariabfer <maria.barbosa@aluno.ufca.edu.br>
2026-03-17 13:56:30 -0300

    alimentada atabelas

--- database/ddl.sql
+++ database/ddl.sql
@@ -19,12 +19,11 @@ DROP TABLE anamnese;
 CREATE TABLE anamnese (
     id_anamnese INTEGER PRIMARY KEY AUTOINCREMENT,
     num_sus INTEGER NOT NULL,
-    id_consulta INTEGER NOT NULL,
     peso REAL NOT NULL,
     altura REAL NOT NULL,
+    data_anaminese TEXT NOT NULL,
     presao_arterial REAL NOT NULL,
-    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus),
-    FOREIGN KEY (id_consulta) REFERENCES consulta(id_consulta)
+    FOREIGN KEY (num_sus) REFERENCES cidadao(num_sus)
 );
 
 DROP TABLE cidadao;
@@ -147,12 +146,14 @@ CREATE TABLE fila (
     data_fila TEXT NOT NULL,
     id_ubs INTEGER NOT NULL,
     tipo_atendimento TEXT NOT NULL,
-    quantidade_maxima INTEGER NOT NULL,
+    quantidade_maxima INTEGER NOT NULL CHECK (quantidade_maxima > 0),
     crm INTEGER,
     id_vacina INTEGER,
+    
     FOREIGN KEY (id_ubs) REFERENCES ubs(id_ubs),
     FOREIGN KEY (crm) REFERENCES medico(crm),
     FOREIGN KEY (id_vacina) REFERENCES vacina(id_vacina),
+
     CHECK (
         (crm IS NOT NULL AND id_vacina IS NULL) OR
         (id_vacina IS NOT NULL AND crm IS NULL)
