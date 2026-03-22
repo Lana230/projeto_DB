5d2202f7fb6717e1f00ef8976671f563a0d50ed0
mariabfer <maria.barbosa@aluno.ufca.edu.br>
2026-03-22 00:23:42 -0300

    criei o CRUD nele ta a conccao com o banco de dados e nele vai ter os selects update e delets e inserts

--- CRUD/teste_coneccao.py
+++ CRUD/teste_coneccao.py
@@ -91,49 +91,4 @@ finally:
         print("\nConexão SQLite encerrada")
 
 
-#insercão de dados no banco com dados fornecidos por input do usuário
 
-import sqlite3
-
-print("Digite os dados para inserir na tabela pessoa:")
-id_ubs = int(input("ID da UBS: "))
-nome_pessoa = input("Nome da pessoa: ")
-estado_civil = input("Estado civil: ")
-
-dados = (id_ubs, nome_pessoa, estado_civil)
-
-print(dados)
-
-try:
-    con = sqlite3.connect('projeto_DB/database/ubs_teste.db')
-    print("Conexão com SQLite bem-sucedida!")
-    cursor = con.cursor()
-    inserir_pessoa = """
-    INSERT INTO pessoa (id_ubs, nome_pessoa, estado_civil)
-    VALUES (?, ?, ?)
-    """
-
-    cursor.execute(inserir_pessoa, dados)
-
-    con.commit()
-
-    print("Registro inserido com sucesso!")
-
-    consulta_sql = "SELECT * FROM pessoa;"
-    cursor.execute(consulta_sql)
-
-    linhas = cursor.fetchall()
-
-    print("\nNúmero total de registros:", len(linhas))
-    print("\nMostrando os dados:\n")
-
-    for linha in linhas:
-        print(linha)
-
-except sqlite3.Error as e:
-    print("Erro ao acessar banco:", e)
-
-finally:
-    if con:
-        con.close()
-        print("\nConexão SQLite encerrada")
