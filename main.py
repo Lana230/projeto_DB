7941e4dfdf86e1a6ebb446d0b8dc45f3c7255789
Ana Alice <analiceferza2025@gmail.com>
2026-03-26 13:40:40 -0300

    receber atualizacao

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def criar_usuario():
         else:
             print("Opção inválida!")
     
-    ubs = ubs_repo.search_per_name(nome_ubs)
+        ubs = ubs_repo.search_per_name(nome_ubs)
 
     usuario = Usuario(ubs, nome_usuario, email, senha, tipo)
 
